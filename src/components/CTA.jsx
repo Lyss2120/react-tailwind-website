7edb0725649f75584aedfba594ae561762acb343
Lyss2120 <lys.ivonne@gmail.com>
2024-04-13 23:49:27 -0400

    footer added

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Button from "./Button";
+import styles from "../style";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <section
+      className={`${styles.padding} ${styles.flexCenter} ${styles.marginY} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className={`${styles.heading2}`}>Let’s try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
 
-export default CTA
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-0`}>
+        <Button />
+      </div>
+    </section>
+  );
+};
+
+export default CTA;
